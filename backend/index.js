26114a084d7c75c17ab34821311119779176fec7
Jamalbarhoum <jamallbarhoum@gmail.com>
2024-02-08 01:57:47 +0300

    DB

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,15 @@
-console.log("aa");
+const express = require("express");
+
+require("dotenv").config();
+const db = require("./models/db")
+const app = express();
+const PORT = process.env.PORT;
+
+
+app.use(express.json());
+
+app.use("*", (req, res) => res.status(404).json("NO content at this path"));
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
