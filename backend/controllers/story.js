785ce851c1c0e75d930628d9bd4f774d10e8f5b0
Jamalbarhoum <jamallbarhoum@gmail.com>
2024-02-09 22:22:39 +0300

    add Story

--- backend/controllers/story.js
+++ backend/controllers/story.js
@@ -1,16 +1,40 @@
-const {pool} =require("../models/db")
+const { pool } = require("../models/db");
 
+// user_id INT,
+// media_url TEXT,
+// video_url TEXT,
 
+// INSERT INTO
+//     Notifications (user_id, content)
+// VALUES
+//     (123, 'Someone liked your post.');
 
-const addStory = (req,res)=>{
-    const user_id = req.token.user_id;
+const addStory = (req, res) => {
+  const user_id = req.token.user_id;
+  const { video_url } = req.body;
+  const query = `INSERT INTO Stories (user_id,video_url) VALUES ($1,$2) RETURNING *`;
+  console.log();
+  const values = [user_id, video_url];
 
-    
+  console.log(values);
 
+  pool
+    .query(query, values)
+    .then((result) => {
+      res.status(201).json({
+        message: "created Successful Story",
+        result: result.rows,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Server error",
+        err: err,
+      });
+    });
+};
+const removeStory = (req, res) => {
+   
+};
 
-}
-const removeStory = (req,res)=>{
-
-}
-
-module.exports = {addStory,removeStory}
+module.exports = { addStory, removeStory };
