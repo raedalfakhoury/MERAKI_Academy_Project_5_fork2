1cb1907c5306cabb942f5309dcea8fbe7935f46e
Mustafamuonther <mustafa.mounther@gmail.com>
2024-02-10 16:27:04 +0300

    get

--- backend/controllers/Reels.js
+++ backend/controllers/Reels.js
@@ -1,39 +1,40 @@
 const { pool } = require("../models/db");
- 
+
 const createNewReels = (req, res) => {
 
-    const {description  , video_url } =req.body
+    const { description, video_url } = req.body
     const user_id = req.token.user_id;
-    const query =`INSERT INTO Reels (user_id, description, video_url ) 
+    const query = `INSERT INTO Reels (user_id, description, video_url ) 
     VALUES ($1, $2, $3) 
-    RETURNING id, user_id, description, video_url, creation_date` 
-    ;
-    const data =[user_id, description, video_url] ;
-    
-    pool
-    .query(query,data)
-    .then((result)=>{
-        res.status(201).json({
-            success:true ,
-            message:"Reel created successfully ",
-            result:result.rows[0], 
-        });
-    })
-    .catch((err)=>{
-        console.log(err);
-        res.status(500).json({
-            success:false ,
-            message:"server error",
-            result:err.message, 
-        })
+    RETURNING id, user_id, description, video_url, creation_date`
+        ;
+    const data = [user_id, description, video_url];
 
-    })
+    pool
+        .query(query, data)
+        .then((result) => {
+            res.status(201).json({
+                success: true,
+                message: "Reel created successfully ",
+                result: result.rows[0],
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({
+                success: false,
+                message: "server error",
+                result: err.message,
+            })
+
+        })
 
 };
 
 
+
 const deleteReels = (req, res) => {
-    
+
     const Reels_id = req.params.id;
     const query = `UPDATE Reels SET is_deleted = 1 WHERE id=${Reels_id} RETURNING *`;
 
@@ -56,6 +57,29 @@ const deleteReels = (req, res) => {
         });
 };
 
+const getReelsByUser = (req, res) => {
+    const id = req.params.id
+    const query = `SELECT * FROM Reels WHERE user_id = ${id} AND is_deleted=0`
+    pool.query(query).then((result) => {
+        if (result.rows.length === 0) {
+            res.status(404).json({
+                message: "reels not exist",
+                result: result.rows
+            })
+            return
+        }
+        res.status(200).json({
+            message: "Succesful reels by user",
+            result: result.rows
+        })
+    }).catch((err) => {
+        res.status(500).json({
+            success: false,
+            message: "Server error",
+            result: err.message,
+        });
+    })
+}
 
 
 
@@ -66,5 +90,4 @@ const deleteReels = (req, res) => {
 
 
 
-
-module.exports = {createNewReels,deleteReels}
+module.exports = { createNewReels, deleteReels, getReelsByUser }
