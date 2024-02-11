445dd7461f3f8f193d03a23d021405733ef3f2a4
raedalfakhoury <alfakhouryraed@gmail.com>
2024-02-11 20:57:58 +0300

    get coordinates function

--- frontend/src/components/Weather/Weather.js
+++ frontend/src/components/Weather/Weather.js
@@ -1,6 +1,17 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import { React, useEffect, useState } from 'react'
+import axios from 'axios';
+ 
 
 function Weather() {
+  const getCoordinates = async () => {
+    return await new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(
+        (position) => resolve(position),
+        (error) => reject(error)
+      );
+    });
+  }
   return (
     <div>Weather</div>
   )
