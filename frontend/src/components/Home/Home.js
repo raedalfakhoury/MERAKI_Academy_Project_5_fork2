d0c20ed0e3a8a42f10ca6dbe24c871e0e1dbc916
Jamalbarhoum <jamallbarhoum@gmail.com>
2024-02-11 20:15:58 +0300

    start code frontend

---
+++ frontend/src/components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from "../Navbar/Navbar"
+import Weather from "../Weather/Weather"
+
+function Home() {
+  return (
+    <>
+   <Navbar/>
+   <Weather/>
+   <div>Home</div>
+    
+    </>
+  
+  )
+}
+
+export default Home
