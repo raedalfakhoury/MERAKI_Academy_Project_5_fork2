80c506007210e7f97ad7436d2af126d6cae950d9
Mohammmad Kreishan <146862386+MohammedKreishan@users.noreply.github.com>
2024-02-12 17:44:13 +0300

    Create Login And Register Pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,34 @@
 import "./App.css";
 import Home from "./components/Home/Home";
-
+import { Routes, Route, Link } from "react-router-dom";
+import {
+  Navbar,
+  Container,
+  Nav,
+  Form,
+  FormControl,
+  Dropdown,
+} from "react-bootstrap";
+import Rigester from "./components/Rigester";
+import LoginPage from "./components/Login";
 function App() {
   return (
     <div className="App">
-      <h1>jamal</h1>
-      <Home />
+
+
+      <Routes>
+        <Route path="/users/Rigester" element={<Rigester/>} />
+        <Route path="/users/login" element={<LoginPage/>} />
+
+      </Routes>
+      <h1>
+      <Nav.Link style={{ margin: "8px" }} href="/users/Rigester">
+      Rigester
+      </Nav.Link></h1>
+      <h1>
+      <Nav.Link style={{ margin: "8px" }} href="/users/login">
+      Login
+      </Nav.Link></h1>
     </div>
   );
 }
--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from "../Navbar/Navbar"
 import Weather from "../Weather/Weather"
+import Register from '../Rigester'
+import LoginPage from '../Login'
 
 function Home() {
   return (
     <>
    <Navbar/>
    <Weather/>
+   <Register/>
+   <LoginPage/>
    <div>Home</div>
     
     </>
