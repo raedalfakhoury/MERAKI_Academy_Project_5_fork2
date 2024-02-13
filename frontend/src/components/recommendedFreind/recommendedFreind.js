6c0432f110ff136916615484c61363d36f0dfc8c
raedalfakhoury <alfakhouryraed@gmail.com>
2024-02-13 22:45:20 +0300

    start on recommended freind s 50%

--- frontend/src/components/recommendedFreind/recommendedFreind.js
+++ frontend/src/components/recommendedFreind/recommendedFreind.js
@@ -5,6 +5,7 @@ import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import usersSlice, { setUsers } from "../redux/reducers/users/users";
 import "../recommendedFreind/recommendedFreind.css";
+import { GoPersonAdd } from "react-icons/go";
 const RecommendedFreind = () => {
   const dispatch = useDispatch();
   const { users } = useSelector((state) => {
@@ -29,10 +30,17 @@ const RecommendedFreind = () => {
 
   return (
     <div className="mainRecommended">
+      <h3 className="suggested-freinds">Suggested Freinds</h3>
       {users?.slice(0,5)?.map((item, i) => {
-        console.log(item);
         return <div key={item.i} className="panel">
-          <img  key={i} className="profileImg" alt="" src={item.profile_picture_url}   style={{ width: '50px', height: '50px', borderRadius: '50%' }}></img>
+        <div className="all-info">
+        <img  key={i} className="profileImg" alt="" src={item.profile_picture_url}   style={{ width: '50px', height: '50px', borderRadius: '50%' }}></img>
+          <div className="name-bio">
+            <p className="name">{item.username}</p>
+            <p className="bio">{item.bio}</p>
+          </div>
+        </div>
+        <GoPersonAdd className="person"/>
         </div>
       })}
     </div>
