dce909b4f87dfec79c1e5910a7d8b0acd688c4ab
Mohammmad Kreishan <146862386+MohammedKreishan@users.noreply.github.com>
2024-02-15 00:09:49 +0300

    Routers >> Done

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,35 +1,36 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import Navbar from "../Navbar/Navbar"
-import Weather from "../Weather/Weather"
-import RecommendedFreind from '../recommendedFreind/recommendedFreind'
-import Register from '../Rigester'
-import LoginPage from '../Login'
-import Post from '../Post/Post'
-import WelcomePage from '../welcome page/welcomepage'
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
+import Weather from "../Weather/Weather";
+import RecommendedFreind from "../recommendedFreind/recommendedFreind";
+import Register from "../Rigester/index";
+import LoginPage from "../Login/index";
+import Post from "../Post/Post";
+import WelcomePage from "../welcome page/welcomepage";
 
 function Home() {
   return (
     <>
-     {/* <WelcomePage/>  */}
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/users/register" element={<Register />} />
+        <Route path="/users/login" element={<LoginPage />} />
+        {/* <Route path="/users/aboutUs" element={<AboutUs />} /> */}
+        {/* <Route path="/users/dashboard" element={<Dashboard />} /> */}
+        {/* <Route path="/users/admin" element={<Admin />} /> */}
+      </Routes>
+      {/* <WelcomePage/>  */}
 
-    {/* <Navbar/> */}
-   {/* <Weather/> */}
-{/* <RecommendedFreind/> */}
-   {/* <Register/> */}
-   <LoginPage/>
-   {/* <div>Home</div>   */}
-   {/* <Post/>   */}
- 
-   
-  
-    
-   
- 
-
+      {/* <Navbar/> */}
+      {/* <Weather/> */}
+      {/* <RecommendedFreind/> */}
+      <Register/>
+      {/* <LoginPage /> */}
+      {/* <div>Home</div>   */}
+      {/* <Post/>   */}
     </>
-  
-  )
+  );
 }
 
-export default Home
+export default Home;
