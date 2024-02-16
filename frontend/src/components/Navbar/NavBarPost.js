9f6a17895d131e99bd801d859acd76eef43cac81
Mohammmad Kreishan <146862386+MohammedKreishan@users.noreply.github.com>
2024-02-17 00:38:41 +0300

    NavBar in post page >> Fixed navbar && Modify Navbar hight

--- frontend/src/components/Navbar/NavBarPost.js
+++ frontend/src/components/Navbar/NavBarPost.js
@@ -9,6 +9,8 @@ import {
   Nav,
   Button,
   NavDropdown,
+  NavbarCollapse,
+  Form,
 } from "react-bootstrap";
 import "./style.css";
 
@@ -23,122 +25,13 @@ const NavBarPost = () => {
     setIsHovered(false);
   };
   return (
-    // <header
-    //   style={{
-    //     paddingTop: "5px",
-    //     background: "#FFFF",
-    //     color: "white",
-    //     marginBottom: "20px",
-    //   }}
-    // >
-    //   <Container>
-    //     <Navbar className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-    //       <Navbar.Brand
-    //         href="/Home"
-    //         className="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none"
-    //       ></Navbar.Brand>
-
-    //       {/* Heart Notifications  */}
-
-    //       {/* Bill Notifications  */}
-    //       <Nav className="col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-    //         <Nav.Link
-    //           href="/Home"
-    //         >
-    //           <Button
-    //             className="position-relative navbar"
-    //             variant="light"
-    //           >
-
-    //             <Badge
-    //               pill
-    //               bg="danger"
-    //               className="position-absolute top-0 start-100 translate-middle"
-    //             >
-    //               99+<span className="visually-hidden">unread messages</span>
-    //             </Badge>
-    //           </Button>
-    //         </Nav.Link>
-    //       </Nav>
-
-    //     {/* Bill Notifications  */}
-    //       <Nav className="col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-    //         <Nav.Link
-    //           style={{ color: isHovered ? "red" : "black" }}
-    //           href="/Home"
-    //         >
-    //           <Button
-    //             className="position-relative"
-    //             variant="light"
-    //             onMouseEnter={handleMouseEnter}
-    //             onMouseLeave={handleMouseLeave}
-    //           >
-
-    //             <Badge
-    //               pill
-    //               bg="danger"
-    //               className="position-absolute top-0 start-100 translate-middle"
-    //             >
-    //               99+<span className="visually-hidden">unread messages</span>
-    //             </Badge>
-    //           </Button>
-    //         </Nav.Link>
-    //       </Nav>
-
-    //     {/* Message Notifications  */}
-    //       <Nav className="col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-    //         <Nav.Link
-    //           style={{ color: isHovered ? "red" : "black" }}
-    //           href="/Home"
-    //         >
-    //           <Button
-    //             className="position-relative"
-    //             variant="light"
-    //             onMouseEnter={handleMouseEnter}
-    //             onMouseLeave={handleMouseLeave}
-    //           >
-    //             <svg
-    //               xmlns="http://www.w3.org/2000/svg"
-    //               width="16"
-    //               height="16"
-    //               fill="currentColor"
-    //               className="bi bi-heart"
-    //               viewBox="0 0 16 16"
-    //             >
-    //               <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
-    //             </svg>
-    //             <Badge
-    //               pill
-    //               bg="danger"
-    //               className="position-absolute top-0 start-100 translate-middle"
-    //             >
-    //               99+<span className="visually-hidden">unread messages</span>
-    //             </Badge>
-    //           </Button>
-    //         </Nav.Link>
-    //       </Nav>
-
-    //       <Nav.Link style={{ color: "red" }} href="/users/aboutus">
-    //         About Us
-    //       </Nav.Link>
-    //       <Nav.Link style={{ margin: "8px", color: "red" }} href="/users/login">
-    //         Login
-    //       </Nav.Link>
-    //       <Button
-    //         variant="outline-light"
-    //         style={{ fontSize: "1rem" }}
-    //         href="/users/register"
-    //       >
-    //         Register
-    //       </Button>
-    //     </Navbar>
-    //   </Container>
     <header
       style={{
         paddingTop: "5px",
         background: "#FFFF",
         color: "blake",
         marginBottom: "20px",
+        height: "55px",
       }}
     >
       <Navbar
@@ -150,6 +43,10 @@ const NavBarPost = () => {
           background: "#FFFF",
           color: "white",
           marginBottom: "20px",
+          position: "fixed",
+          height: "55px",
+          width:"100%",
+          zIndex:"10"
         }}
       >
         <Container>
@@ -162,6 +59,7 @@ const NavBarPost = () => {
               loading="lazy"
             />
           </Navbar.Brand>
+
           <Navbar.Collapse id="navbarSupportedContent">
             <Nav className="me-auto">
               {/* Heart Notifications */}
@@ -176,10 +74,15 @@ const NavBarPost = () => {
                       xmlns="http://www.w3.org/2000/svg"
                       width="20"
                       height="20"
-                      fill="currentColor"
-                      className="bi bi-heart"
+                      viewBox="0 0 24 24"
+                      fill="#ffff"
+                      stroke="black"
+                      stroke-width="1"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      class="feather feather-heart"
                     >
-                      <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
+                      <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
                     </svg>
 
                     <Badge
@@ -187,8 +90,7 @@ const NavBarPost = () => {
                       bg="danger"
                       className="position-absolute top-0 start-100 translate-middle"
                     >
-                      99+
-                      <span className="visually-hidden">unread messages</span>
+                      6<span className="visually-hidden">unread messages</span>
                     </Badge>
                   </Button>
                 </Nav.Link>
@@ -277,8 +179,8 @@ const NavBarPost = () => {
                       fill="#ffff"
                       stroke="black"
                       stroke-width="1"
-                    //   stroke-linecap="butt"
-                    //   stroke-linejoin="miter"
+                      //   stroke-linecap="butt"
+                      //   stroke-linejoin="miter"
                       class="feather feather-message-square"
                     >
                       <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
@@ -294,56 +196,23 @@ const NavBarPost = () => {
                   </Button>
                 </Nav.Link>
               </Nav>{" "}
-              <Nav className="col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                <Nav.Link href="/Home">
-                  <Button
-                    className="position-relative border border-0"
-                    variant="outline-danger"
-                    border="none"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="20"
-                      height="20"
-                      viewBox="0 0 24 24"
-                      fill="#ffff"
-                      stroke="black"
-                      stroke-width="1"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="feather feather-message-square"
-                    >
-                      <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
-                    </svg>
-                    <Badge
-                      pill
-                      bg="danger"
-                      className="position-absolute top-0 start-100 translate-middle"
-                    >
-                      99+
-                      <span className="visually-hidden">unread messages</span>
-                    </Badge>
-                  </Button>
-                </Nav.Link>
-              </Nav>{" "}
             </Nav>
 
-            {/* Bill Notifications*/}
+            {/* DropDown section*/}
             <Nav className="align-items-center">
+              <NavbarCollapse>
+                <Form.Control
+                  type="text"
+                  placeholder="Search"
+                  className=" mr-sm-2"
+                  width={"1px"}
+                  height={"10px"}
+                />
+              </NavbarCollapse>
               <Nav.Link href="#">
                 <i className="fas fa-shopping-cart"></i>
               </Nav.Link>
-              <NavDropdown
-                title={<i className="fas fa-bell"></i>}
-                id="navbarDropdownMenuLink"
-                align="end"
-              >
-                <NavDropdown.Item href="#">Some news</NavDropdown.Item>
-                <NavDropdown.Item href="#">Another news</NavDropdown.Item>
-                <NavDropdown.Item href="#">
-                  Something else here
-                </NavDropdown.Item>
-              </NavDropdown>
+
               <NavDropdown
                 title={
                   <img
