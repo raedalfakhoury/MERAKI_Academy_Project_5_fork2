d75d883e25d70337d341ee05aed65e15ac02c5ea
Mohammmad Kreishan <146862386+MohammedKreishan@users.noreply.github.com>
2024-02-17 09:32:15 +0300

    Create NavBar By Using MUI >> 50% Done

--- frontend/src/components/Navbar/NavBarPost.js
+++ frontend/src/components/Navbar/NavBarPost.js
@@ -242,60 +242,65 @@
 // };
 
 // export default NavBarPost;
-import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import Badge from '@mui/material/Badge';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import MailIcon from '@mui/icons-material/Mail';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import MoreIcon from '@mui/icons-material/MoreVert';
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import Badge from "@mui/material/Badge";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import MailIcon from "@mui/icons-material/Mail";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import MoreIcon from "@mui/icons-material/MoreVert";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import AddAPhotoIcon from "@mui/icons-material/AddAPhoto";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import "./style.css";
 
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
   borderRadius: theme.shape.borderRadius,
   backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
+  "&:hover": {
     backgroundColor: alpha(theme.palette.common.white, 0.25),
   },
   marginRight: theme.spacing(2),
   marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
     marginLeft: theme.spacing(3),
-    width: 'auto',
+    width: "auto",
   },
 }));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
+const SearchIconWrapper = styled("div")(({ theme }) => ({
   padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
+  color: "inherit",
+  "& .MuiInputBase-input": {
     padding: theme.spacing(1, 1, 1, 0),
     // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
     },
   },
 }));
@@ -324,19 +329,19 @@ export default function NavBarPost() {
     setMobileMoreAnchorEl(event.currentTarget);
   };
 
-  const menuId = 'primary-search-account-menu';
+  const menuId = "primary-search-account-menu";
   const renderMenu = (
     <Menu
       anchorEl={anchorEl}
       anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       id={menuId}
       keepMounted
       transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       open={isMenuOpen}
       onClose={handleMenuClose}
@@ -346,43 +351,44 @@ export default function NavBarPost() {
     </Menu>
   );
 
-  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const mobileMenuId = "primary-search-account-menu-mobile";
   const renderMobileMenu = (
     <Menu
       anchorEl={mobileMoreAnchorEl}
       anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       id={mobileMenuId}
       keepMounted
       transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       open={isMobileMenuOpen}
       onClose={handleMobileMenuClose}
     >
       <MenuItem>
         <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-          <Badge badgeContent={4} color="error">
-            <MailIcon />
+          <Badge badgeContent={9} color="error">
+            <NotificationsIcon />
           </Badge>
         </IconButton>
         <p>Messages</p>
       </MenuItem>
+
       <MenuItem>
         <IconButton
           size="large"
           aria-label="show 17 new notifications"
           color="inherit"
         >
-          <Badge badgeContent={17} color="error">
-            <NotificationsIcon />
-          </Badge>
+          <Badge badgeContent={17} color="error"></Badge>
         </IconButton>
+
         <p>Notifications</p>
       </MenuItem>
+
       <MenuItem onClick={handleProfileMenuOpen}>
         <IconButton
           size="large"
@@ -399,8 +405,8 @@ export default function NavBarPost() {
   );
 
   return (
-    <Box  sx={{ flexGrow: 1 ,}} >
-      <AppBar position="static"  sx={{ bgcolor:"#ffff",color:"black"}}>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{ bgcolor: "#ffff", color: "black" }}>
         <Toolbar>
           {/* <IconButton
             size="large"
@@ -415,48 +421,94 @@ export default function NavBarPost() {
             variant="h6"
             noWrap
             component="div"
-            sx={{ display: { xs: 'none', sm: 'block',color:"blue"} }}
+            sx={{ display: { xs: "none", sm: "block", color: "blue" } }}
           >
             TALAGI
           </Typography>
+
+          {/* Notifications Box */}
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            {/* Likes Notifications */}
+            <IconButton
+              sx={{ "&:hover": { backgroundColor: "red" } }}
+              size="large"
+              aria-label="show 4 new mails"
+              color="inherit"
+              startIcone={<AddAPhotoIcon />}
+              endIcone={<FavoriteBorderIcon />}
+            >
+              <Badge badgeContent={8} color="error">
+                <AddAPhotoIcon />
+              </Badge>
+            </IconButton>
+
+            {/* Message Notifications */}
+            <IconButton
+              sx={{ "&:hover": { backgroundColor: "red" } }}
+              size="large"
+              aria-label="show 4 new mails"
+              color="inherit"
+            >
+              <Badge badgeContent={8} color="error">
+                <FavoriteBorderIcon />
+              </Badge>
+            </IconButton>
+
+            {/* Bill Notifications */}
+            <IconButton
+              size="large"
+              aria-label="show 4 new mails"
+              color="inhert"
+            >
+              <Badge badgeContent={8} color="error">
+                <MailOutlineIcon />
+              </Badge>
+            </IconButton>
+
+            {/* Chat Notifications */}
+            <IconButton
+              sx={{ "&:hover": { backgroundColor: "red" } }}
+              size="large"
+              aria-label="show 17 new notifications"
+              color="inherit"
+            >
+              <Badge badgeContent={17} color="info">
+                <ChatBubbleOutlineIcon />
+              </Badge>
+            </IconButton>
+
+            {/* Message Notifications */}
+            <IconButton
+              sx={{ "&:hover": { backgroundColor: "red" } }}
+              size="large"
+              aria-label="show 17 new notifications"
+              color="inherit"
+            >
+              <Badge
+                badgeContent={17}
+                color="info"
+                style={{ "&:hover": { backgroundColor: "green" } }}
+              >
+                <ChatBubbleOutlineIcon />
+              </Badge>
+            </IconButton>
+          </Box>
+          {/* Space Box */}
+          <Box sx={{ flexGrow: 1 }} />
+          {/* Search */}
           <Search>
             <SearchIconWrapper>
               <SearchIcon />
             </SearchIconWrapper>
             <StyledInputBase
               placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
+              inputProps={{ "aria-label": "search" }}
             />
           </Search>
-          <Box sx={{ flexGrow: 1 }} />
-          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-            <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-              <Badge badgeContent={4} color="error">
-                <MailIcon />
-              </Badge>
-            </IconButton>
-            <IconButton
-              size="large"
-              aria-label="show 17 new notifications"
-              color="inherit"
-            >
-              <Badge badgeContent={17} color="error">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-            <IconButton
-              size="large"
-              edge="end"
-              aria-label="account of current user"
-              aria-controls={menuId}
-              aria-haspopup="true"
-              onClick={handleProfileMenuOpen}
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
-          </Box>
-          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+          {/* Profile Picture */}
+          <AccountCircle />
+
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
               aria-label="show more"
@@ -470,6 +522,7 @@ export default function NavBarPost() {
           </Box>
         </Toolbar>
       </AppBar>
+
       {renderMobileMenu}
       {renderMenu}
     </Box>
