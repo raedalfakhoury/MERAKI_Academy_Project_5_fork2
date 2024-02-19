ad3f332086dd48a756acb20c428f684fdb327dc3
raedalfakhoury <alfakhouryraed@gmail.com>
2024-02-19 08:07:30 +0300

    profile 95%

--- backend/routes/followers.js
+++ backend/routes/followers.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const followersRouter = express.Router();
-const { addFollowers, getAllFollwers ,deleteFollowed , suggestedFreings ,getAllFollowing,getAllFollowers} = require("../controllers/followers");
+const { addFollowers, getAllFollwers ,deleteFollowed , suggestedFreings ,getAllFollowing,getAllFollowers,deleteFollowers} = require("../controllers/followers");
 const authentication = require("../middlewares/authentication");
 followersRouter.post("/add",authentication ,addFollowers);
 // followersRouter.get("/", authentication, getAllFollwers);
 followersRouter.get("/suggested",authentication ,  suggestedFreings);
 followersRouter.get("/Following/:id" ,  getAllFollowing);
 followersRouter.get("/Followers/:id" ,  getAllFollowers);
-followersRouter.delete("/delete", authentication, deleteFollowed);
+followersRouter.delete("/delete",   deleteFollowed);
+followersRouter.delete("/delete/follower/",  deleteFollowers);
 
 module.exports = followersRouter;
