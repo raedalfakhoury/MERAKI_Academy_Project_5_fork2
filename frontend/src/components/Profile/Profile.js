86a64d0aa4c7d53c0286b96b335c85cb2f8698cd
raedalfakhoury <alfakhouryraed@gmail.com>
2024-02-23 21:44:40 +0300

    finish of edit profile

--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -12,11 +12,10 @@ import Loader from "../Loader/Loader";
 import "../Profile/Profile.css";
 import { CiBookmark } from "react-icons/ci";
 import { BsPostcard } from "react-icons/bs";
-import Avatar from "@mui/material/Avatar";
 import { useSelector } from "react-redux";
 import Modal from "react-bootstrap/Modal";
-import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
+import Swal from "sweetalert2";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 import { styled } from "@mui/material/styles";
 const VisuallyHiddenInput = styled("input")({
@@ -35,6 +34,7 @@ const Profile = () => {
   const arr = useRef([]);
   const [remove, setRemove] = useState(false);
   const [test, setTest] = useState(true);
+  let bio = useRef("");
   const { token } = useSelector((state) => {
     return {
       token: state.auth.token,
@@ -106,7 +106,6 @@ const Profile = () => {
             },
           })
           .then((result) => {
-            console.log(result);
             result?.data?.result.forEach((ele) => {
               if (ele.relationship_type == "following") {
                 serCountFollowing(ele.count);
@@ -124,7 +123,7 @@ const Profile = () => {
       .catch((err) => {
         console.log(err);
       });
-  }, [searchQuery2, arr]);
+  }, [searchQuery2]);
 
   function Followers() {
     return (
@@ -203,6 +202,7 @@ const Profile = () => {
                               return ele.id !== item.id;
                             })
                           );
+                          serCountFollowers(countFollowers - 1);
                         } catch (error) {
                           console.log(error);
                         }
@@ -213,7 +213,7 @@ const Profile = () => {
                   ) : // {لما اكون ببروفايل شخص اخر ويظهر امي من ضمن المتابعين له لا اريد ان يظهر زر الحذف}
                   localStorage.getItem("userId") ==
                     // {اذا كان الشخص يلي بتابعني من ضمن المصفوفه يلي فيها الاشخاص يلي بتابعهم}
-                    item.id ? null : arr.current?.includes(item.id) && test ? (
+                    item.id ? null : arr.current?.includes(item.id) ? (
                     <button
                       className="button-pop"
                       onClick={async () => {
@@ -229,11 +229,14 @@ const Profile = () => {
                             }
                           );
                           // console.log("t.a", res.data.result);
-                          filtration = followers.filter((ele) => {
-                            return ele.id !== res.data.result[0].followed_id;
-                          });
+                          // filtration = followers.filter((ele) => {
+                          //   return ele.id !== res.data.result[0].followed_id;
+                          // });
                           // setFollowers(filtration);
-                          arr.current = filtration;
+                          arr.current = arr.current.filter((ele) => {
+                            return ele !== item.id * 1;
+                          });
+                          // arr.current = filtration;
                           setTest(!test);
                         } catch (error) {
                           console.log(error);
@@ -466,37 +469,81 @@ const Profile = () => {
             id="Modal.Body"
             style={{
               padding: "10px",
-              height: following?.length == 0 ? "100px" : "525px",
+              display: "flex",
               overflowY: "auto",
             }}
           >
             <div className="mainEditProfile">
-              {user?.map((ele) => {
-                console.log(ele);
-
+              {user?.map((ele, i) => {
                 return (
-                  // <img  className="imgEditProfile" alt="" src={ele.profile_picture_url} />
-                  <Stack direction="row" spacing={2}>
-                    <Avatar
-                      alt={ele.username}
+                  <>
+                    <img
+                      key={i}
+                      className="imgEditProfile"
+                      alt=""
                       src={ele.profile_picture_url}
-                      sx={{ width: 200, height: 200 }}
                     />
-                  </Stack>
+                    <div>
+                      <Button
+                        component="label"
+                        role={undefined}
+                        variant="contained"
+                        tabIndex={-1}
+                        startIcon={<CloudUploadIcon />}
+                      >
+                        Upload image
+                        <VisuallyHiddenInput type="file" />
+                      </Button>
+                    </div>
+                    <textarea
+                      id="w3review"
+                      name="w3review"
+                      rows="4"
+                      cols="50"
+                      placeholder={ele.bio}
+                      onChange={(e) => {
+                        bio = e.target.value;
+                      }}
+                    ></textarea>
+
+                    <button
+                      id="btn2"
+                      onClick={async () => {
+                        try {
+                          const result = await axios.put(
+                            `http://localhost:5000/users/update`,
+                            {
+                              bio: bio,
+                            },
+                            {
+                              headers: {
+                                Authorization: `Bearer ${token}`,
+                              },
+                            }
+                          );
+                          Swal.fire({
+                            position: "center",
+                            icon: "success",
+                            title: "Successfully changed",
+                            showConfirmButton: false,
+                            timer: 1000,
+                          });
+                          const edit = profileInfo.map((ele) => {
+                            ele.bio = bio;
+                            return ele
+                          });
+                          setProfileInfo(edit)
+                          handleCloseEditProfile();
+                        } catch (error) {
+                          console.log("error from update profile bio", error);
+                        }
+                      }}
+                    >
+                      SAVE CHANGE
+                    </button>
+                  </>
                 );
               })}
-              <div>
-                <Button
-                  component="label"
-                  role={undefined}
-                  variant="contained"
-                  tabIndex={-1}
-                  startIcon={<CloudUploadIcon />}
-                >
-                  Upload image
-                  <VisuallyHiddenInput type="file" />
-                </Button>
-              </div>
             </div>
           </Modal.Body>
         </Modal>
@@ -526,26 +573,21 @@ const Profile = () => {
                 <div className="follower">
                   <p className="username">{elm.username}</p>
 
-                  {localStorage.getItem("userId") == searchQuery2 ? (
-                    // <p
-                    //   className="followers"
-                    //   style={{ cursor: "pointer" }}
-                    //   onClick={async () => {
-                    //     try {
-                    //       const result = await axios.get(
-                    //         `http://localhost:5000/users/${searchQuery2}`
-                    //       );
-                    //       setUser(result.data.result);
-                    //       handleShowEditProfile();
-                    //     } catch (error) {
-                    //       console.log(error);
-                    //     }
-                    //   }}
-                    // >
-                    //   {" "}
-                    //   Edit profile
-                    // </p>
-                    <button class="Btn">
+                  {localStorage.getItem("userId") == searchQuery2 ? ( 
+                    <button
+                      className="Btn"
+                      onClick={async () => {
+                        try {
+                          const result = await axios.get(
+                            `http://localhost:5000/users/${searchQuery2}`
+                          );
+                          setUser(result.data.result);
+                          handleShowEditProfile();
+                        } catch (error) {
+                          console.log(error);
+                        }
+                      }}
+                    >
                       Edit
                       <svg class="svg" viewBox="0 0 512 512">
                         <path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"></path>
@@ -553,9 +595,9 @@ const Profile = () => {
                     </button>
                   ) : arr.current.includes(searchQuery2) ? (
                     <button
-                      id=" UnFollow-Public"
-                      className="button-pop"
-                      onClick={async () => {
+                      // id=" UnFollow-Public"
+                      className="Btn"
+                      onClick={async () => { 
                         try {
                           const res = await axios.delete(
                             `http://localhost:5000/followers/delete`,
@@ -571,11 +613,10 @@ const Profile = () => {
                             return ele !== searchQuery2;
                           });
                           setRemove(!remove);
-                          {
-                            localStorage.getItem("userId") == searchQuery2 &&
-                              serCountFollowing(countFollowing - 1);
-                          }
-                          console.log("in", arr.current);
+                          // {
+                          //   localStorage.getItem("userId") == searchQuery2 &&
+                          // }
+                          serCountFollowers(countFollowers * 1 - 1);
                         } catch (error) {
                           console.log(error);
                         }
@@ -586,8 +627,8 @@ const Profile = () => {
                     </button>
                   ) : (
                     <button
-                      id="Follow-Public"
-                      className="button-pop"
+                      // id="Follow-Public"
+                      className="Btn"
                       onClick={async () => {
                         if (arr.current.includes(searchQuery2)) {
                         } else {
@@ -603,10 +644,10 @@ const Profile = () => {
                             );
                             arr.current.push(searchQuery2);
                             setRemove(!remove);
-                            {
-                              localStorage.getItem("userId") == searchQuery2 &&
-                                serCountFollowing(countFollowing + 1);
-                            }
+                            // {
+                            //   localStorage.getItem("userId") == searchQuery2 &&
+                            // }
+                            serCountFollowers(countFollowers * 1 + 1);
                           } catch (error) {
                             console.log(error);
                           }
